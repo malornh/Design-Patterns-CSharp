f983df0d6d9c9257aabf144cc3b266622ab3e8d1
Jivko Ivanov <jivko.ivanov@codinext.com>
2024-09-20 16:54:11 +0300

    Add adapter example

---
+++ Design Patterns/Adapter/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.Adapter
+{
+    public class Circle
+    {
+        private double _radius;
+
+        public Circle(double radius)
+        {
+            _radius = radius;
+        }
+
+        public double getRadius()
+        {
+            return _radius;
+        }
+    }
+}
