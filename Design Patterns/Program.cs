4d1189b55b346344e17cb28f99cd5e307fc47d98
Jivko Ivanov <jivko.ivanov@codinext.com>
2024-09-20 13:21:46 +0300

    Add project files.

---
+++ Design Patterns/Program.cs
@@ -0,0 +1,12 @@
+﻿using Design_Patterns.Singleton;
+
+// Singletion 
+
+var globalMood = MoodManager.GetInstance("Happy!");
+Console.WriteLine(globalMood.Mood);
+
+var localMood = MoodManager.GetInstance("Neutral");
+Console.WriteLine(localMood.Mood); // Still Happy!
+
+//
+
---
+++ Design Patterns/Singleton/MoodManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.Singleton
+{
+    class MoodManager
+    {
+        private MoodManager() { }
+
+        private static MoodManager _instance;
+        private static readonly object _lock = new object();
+        public string Mood {  get; private set; }
+
+        public static MoodManager GetInstance(string mood)
+        {
+            if( _instance == null )
+            {
+                lock (_lock) // Thread safe
+                {
+                    if(_instance == null)
+                    {
+                        _instance = new MoodManager();
+                        _instance.Mood = mood;
+                    }
+                }
+            }
+
+            return _instance;
+        }
+
+    }
+}
